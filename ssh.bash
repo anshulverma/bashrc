91593bc790802a7aac8091058c6186e60883ac19
Anshul Verma <anshul.verma86@gmail.com>
2014-10-15 15:10:41 -0700

    fix ssh agent setup script

--- ssh.bash
+++ ssh.bash
@@ -1,11 +1,26 @@
 #!/bin/bash
 
 function latest_key() {
-  echo `ls -1 | tail -n 2 | head -n 1`
+  key_name=`ls -1 $HOME/.ssh/keys | tail -n 2 | head -n 1`
+  echo "$HOME/.ssh/keys/${key_name}"
 }
 
-if [ -z "$SSH_AUTH_SOCK" ] ; then
+function all_keys() {
+  echo $(latest_key)
+
+  # iterate over all keys in $HOME/.ssh
+  for key_name in `ls -1 $HOME/.ssh | grep rsa | grep -v pub`; do
+    echo "$HOME/.ssh/${key_name}"
+  done
+}
+
+if [ -z "$SSH_AUTH_SOCK" ]; then
   echo "Starting ssh-agent..."
   eval `ssh-agent -s`
-  ssh-add $(latest_key)
+
+  for key in $(all_keys); do
+    ssh-add $key
+  done
+else
+  echo "SSH agent already running"
 fi
