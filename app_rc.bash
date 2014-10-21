050fc653c319b7dec7df9082fa5b7a0d15410395
Anshul Verma <anshul.verma86@gmail.com>
2014-10-21 16:01:00 -0700

    adding ability to load all app config defined in /app

--- app_rc.bash
+++ app_rc.bash
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-# load docker configuration
-source $BASEDIR/app/docker.bash
+# Load all application configurations
+for file in `find $BASEDIR/app -name "*.bash" -type f`; do
+  filename=`basename $file | sed 's/.bash$//'`
+  echo -ne "setting up $filename...."
+  startTime=`date +%s`
+  source $file
+  timeTaken=$(expr `date +%s` - $startTime)
+  echo -e "${Green}done${ResetColor} [${timeTaken} msec]"
+done
