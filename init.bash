40fb4cbe5e112729faa522317c47c77038c87b8e
Anshul Verma <anshul.verma86@gmail.com>
2014-10-21 16:01:18 -0700

    fix green color

--- init.bash
+++ init.bash
@@ -7,7 +7,7 @@ Color_Off="\[\033[0m\]"       # Text Reset
 # Regular Colors
 Black="\[\033[0;30m\]"        # Black
 Red="\[\033[0;31m\]"          # Red
-Green="\[\033[0;32m\]"        # Green
+Green="\033[0;32m"        # Green
 Yellow="\[\033[0;33m\]"       # Yellow
 Blue="\[\033[0;34m\]"         # Blue
 Purple="\[\033[0;35m\]"       # Purple
