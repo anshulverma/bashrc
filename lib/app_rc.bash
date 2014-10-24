d80bba3d0ef56f52102c69207c5ae88e15cb1840
Anshul Verma <anshul.verma86@gmail.com>
2014-10-24 10:48:10 -0700

    parallel execution does not export env variables

--- lib/app_rc.bash
+++ lib/app_rc.bash
@@ -2,8 +2,5 @@
 
 # Load all application configurations
 for app in `find $LIB_DIR/app -name "*.bash" -type f`; do
-  loadScript $app &
+  loadScript $app
 done
-
-# wait for all apps to be loaded
-wait
