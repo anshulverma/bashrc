c44ee8904dbca535c263da28d96d234d60a3ddf3
Anshul Verma <anshul.verma86@gmail.com>
2014-11-05 14:47:37 -0800

    making prompt be smarter

--- lib/prompt.bash
+++ lib/prompt.bash
@@ -1,17 +1,7 @@
 #!/bin/bash
 
-Time12h="\T"
-Time12a="\@"
-PathShort="\w"
-PathFull="\W"
-NewLine="\n"
-Jobs="\j"
-
-# export PS1='\u@\h:\w\$ '
-# export PS1="\u@\h:\w$ "
-export PS1="\[\033[36m\]\u\[\033[m\]@\[\033[32m\]\h:\[\033[33;1m\]\w\[\033[m\]\$ "
-
 # enable color support (dependency 'coreutils')
+
 ### Enable colors in terminal
 export CLICOLOR=1
 export LSCOLORS=GxFxCxDxBxegedabagaced
@@ -31,20 +21,95 @@ if ${use_color} ; then
   fi
 fi
 
-PromptResetColor="\[\033[0m\]"
+function print_git_branch() {
+  git branch &>/dev/null;
+  if [ $? -eq 0 ]; then
+    git status | grep "nothing to commit" > /dev/null 2>&1;
+    if [ "$?" -eq "0" ]; then
+      # clean repository - nothing to commit
+      echo -e "$Green$(__git_ps1 "(%s)")${ResetColor} ";
+    else
+      # changes to working tree
+      echo -e "$IRed$(__git_ps1 "(%s)")${ResetColor} ";
+    fi
+  fi
+}
 
-DEFAULT_PROMPT="$Cyan\u@\h:$BYellow$PathShort$PromptResetColor"
-export PS1='$(git branch &>/dev/null;\
-    if [ $? -eq 0 ]; then \
-      echo "$(echo `git status` | grep "nothing to commit" > /dev/null 2>&1; \
-      if [ "$?" -eq "0" ]; then \
-        # @4 - Clean repository - nothing to commit
-        echo "'$Green'"$(__git_ps1 "(%s)"); \
-      else \
-        # @5 - Changes to working tree
-        echo "'$IRed'"$(__git_ps1 "(%s)"); \
-      fi) '$DEFAULT_PROMPT'\$ "; \
-    else \
-      # @2 - Prompt when not in GIT repo
-      echo "'$DEFAULT_PROMPT'\$ "; \
-    fi)'
+# Test connection type:
+if [ -n "${SSH_CONNECTION}" ]; then
+  CNX=${Green}        # Connected on remote machine, via ssh (good).
+elif [[ "${DISPLAY%%:0*}" != "" ]]; then
+  CNX=${ALERT}        # Connected on remote machine, not via ssh (bad).
+else
+  CNX=${Cyan}        # Connected on local machine.
+fi
+
+# Test user type:
+if [[ ${USER} == "root" ]]; then
+  SU=${Red}           # User is root.
+elif [[ ${USER} != $(logname) ]]; then
+  SU=${BRed}          # User is not login user.
+else
+  SU=${Cyan}         # User is normal (well ... most of us are).
+fi
+
+# Number of CPU
+if [ $PLATFORM == 'OSX' ]; then
+  NCPU=$(sysctl -a | grep machdep.cpu | grep core_count | cut -c 25-)
+else
+  NCPU=$(grep -c 'processor' /proc/cpuinfo)
+fi
+
+SLOAD=$(( 100*${NCPU} ))        # Small load
+MLOAD=$(( 200*${NCPU} ))        # Medium load
+XLOAD=$(( 400*${NCPU} ))        # Xlarge load
+
+# Returns system load as percentage, i.e., '40' rather than '0.40)'.
+function load() {
+  if [ $PLATFORM == 'OSX' ]; then
+    local SYSLOAD=$(sysctl -n vm.loadavg | cut -d " " -f2 | tr -d '.')
+  else
+    local SYSLOAD=$(cut -d " " -f1 /proc/loadavg | tr -d '.')
+  fi
+  echo $((10#$SYSLOAD))       # Convert to decimal.
+}
+
+# Returns a color indicating system load.
+function load_color() {
+  local SYSLOAD=$(load)
+  if [ ${SYSLOAD} -gt ${XLOAD} ]; then
+    echo -en ${ALERT}
+  elif [ ${SYSLOAD} -gt ${MLOAD} ]; then
+    echo -en ${Red}
+  elif [ ${SYSLOAD} -gt ${SLOAD} ]; then
+    echo -en ${BRed}
+  else
+    echo -en ${BGreen}
+  fi
+}
+
+# Returns a color according to running/suspended jobs.
+function job_color() {
+  if [ $(jobs -s | wc -l) -gt "0" ]; then
+    echo -en ${BRed}
+  elif [ $(jobs -r | wc -l) -gt "0" ] ; then
+    echo -en ${BCyan}
+  fi
+}
+
+# Now we construct the prompt.
+PROMPT_COMMAND="history -a"
+case ${TERM} in
+  *term | rxvt | linux)
+    PS1="\$(print_git_branch)"
+    # User@Host (with connection type info):
+    PS1=${PS1}"\[${SU}\]\u\[${ResetColor}\]@\[${CNX}\]\h\[${ResetColor}\]"
+    # Prompt (with 'job' info):
+    PS1=${PS1}":\$(load_color)\w${ResetColor}"
+    # ending character '#' with load color
+    PS1=${PS1}" \$(job_color)#${ResetColor} "
+    ;;
+  *)
+    PS1="\[\033[36m\]\u\[\033[m\]@\[\033[32m\]\h:\[\033[33;1m\]\w\[\033[m\]\$ "
+    ;;
+esac
