a3464b3db50ba3b8e53050596b3d68622ad77be4
Anshul Verma <anshul.verma86@gmail.com>
2014-11-05 15:50:52 -0800

    adding a few more util methods

--- lib/util.bash
+++ lib/util.bash
@@ -29,3 +29,67 @@ function load_script() {
   timeTaken=$(expr $(current_time) - $startTime)
   echo -e "....................\r$appName\r\033[20C$status [${timeTaken} msec]"
 }
+
+# ask user for y/n input
+# example use:
+# if ask "are you sure?"; then
+#   do_something
+# fi
+function ask() {
+  echo -n "$@" '[y/n] ' ; read ans
+  case "$ans" in
+    y*|Y*) return 0 ;;
+    *) return 1 ;;
+  esac
+}
+
+#-------------------------------------------------------------
+# File & strings related functions:
+#-------------------------------------------------------------
+
+# Find a file with a pattern in name:
+function ff() {
+  find . -type f -iname '*'"$*"'*' -ls
+}
+
+# Find a file with pattern $1 in name and Execute $2 on it:
+function fe() {
+  find . -type f -iname '*'"${1:-}"'*' \
+       -exec ${2:-file} {} \;
+}
+
+#  Find a pattern in a set of files and highlight them:
+#+ (needs a recent version of egrep).
+function fstr() {
+  OPTIND=1
+  local mycase=""
+  local usage="fstr: find string in files.
+Usage: fstr [-i] \"pattern\" [\"filename pattern\"] "
+  while getopts :it opt
+  do
+    case "$opt" in
+      i) mycase="-i " ;;
+      *) echo "$usage"; return ;;
+    esac
+  done
+  shift $(( $OPTIND - 1 ))
+  if [ "$#" -lt 1 ]; then
+    echo "$usage"
+    return;
+  fi
+  find . -type f -name "${2:-*}" -print0 | \
+    xargs -0 egrep --color=always -sn ${case} "$1" 2>&- | more
+}
+
+# Swap 2 filenames around, if they exist (from Uzi's bashrc).
+function swap() {
+  local TMPFILE=tmp.$$
+
+  [ $# -ne 2 ] && echo "swap: 2 arguments needed" && return 1
+  [ ! -e $1 ] && echo "swap: $1 does not exist" && return 1
+  [ ! -e $2 ] && echo "swap: $2 does not exist" && return 1
+
+  mv "$1" $TMPFILE
+  mv "$2" "$1"
+  mv $TMPFILE "$2"
+}
