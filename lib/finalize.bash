2572fc0d562090078cb144e08e94014deb4e7093
ansverma <ansverma@adobe.com>
2014-11-05 17:09:31 -0800

    make fortune work on linux

--- lib/finalize.bash
+++ lib/finalize.bash
@@ -2,5 +2,10 @@
 
 fortune=$(which fortune)
 if [ ! -z "$fortune" ] && [ -x "$fortune" ]; then
-  $fortune ascii-art computers riddles
+  case $PLATFORM in
+    OSX) $fortune ascii-art computers riddles
+         ;;
+    *)
+         $fortune
+  esac
 fi
