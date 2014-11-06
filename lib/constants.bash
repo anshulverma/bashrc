e1689b376389a0822c3ac117510924f4d0a093c1
ansverma <ansverma@adobe.com>
2014-11-05 16:32:05 -0800

    moving brew related config in one place

--- lib/constants.bash
+++ lib/constants.bash
@@ -3,7 +3,6 @@
 ### environment variable customizations ###
 export JAVA_HOME=`/usr/libexec/java_home -v 1.7`
 export GROOVY_HOME=/usr/local/opt/groovy/libexec
-export CELLAR_PATH=/Users/ansverma/.workspace/homebrew/Cellar
 export HISTCONTROL=ignoredups
 export TMP=/tmp
 export MAVEN_OPTS="-Xmx2048m -XX:MaxPermSize=2048m"
