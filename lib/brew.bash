b2b000d49f49662776ae0ef6b5421a2b07aae5d5
ansverma <ansverma@adobe.com>
2014-11-05 16:56:44 -0800

    brew does not need to be configured in non-osx environments

--- lib/brew.bash
+++ lib/brew.bash
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ $PLATFORM != 'OSX' ]; then
+  EXIT_CODE=1
+  return
+fi
+
 # Load RVM into a shell session *as a function*
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 
