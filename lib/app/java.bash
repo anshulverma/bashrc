ec2ca62d0b4f6b2f7db1b83056924b686736457a
ansverma <ansverma@adobe.com>
2014-11-05 16:42:22 -0800

    fixing java app for linux

--- lib/app/java.bash
+++ lib/app/java.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export JAVA_HOME=`/usr/libexec/java_home -v 1.7`
-export GROOVY_HOME=/usr/local/opt/groovy/libexec
+export JAVA_HOME=${JAVA_HOME:-"$(/usr/libexec/java_home -v 1.7)"}
+export GROOVY_HOME=${GROOVY_HOME:-"/usr/local/opt/groovy/libexec"}
 export MAVEN_OPTS="-Xmx2048m -XX:MaxPermSize=2048m"
