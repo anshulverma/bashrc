e459c141da6adcd4c7b1823ac6cd631b6f276e48
ansverma <ansverma@adobe.com>
2014-11-05 17:04:22 -0800

    only configure git if it is inatalled

--- lib/git.bash
+++ lib/git.bash
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ -z "$(which git)" ]; then
+  EXIT_CODE=1
+  return
+fi
+
 if [ $PLATFORM == 'OSX' ]; then
   # git auto complete
   source $CELLAR_PATH/git/2.1.1/etc/bash_completion.d/git-completion.bash
