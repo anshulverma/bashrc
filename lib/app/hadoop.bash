d9c4bb7e07b0306f33e3b1c5ad1b5aadd60fa1d0
ansverma <ansverma@adobe.com>
2014-11-05 17:02:58 -0800

    make hadoop config available for override

--- lib/app/hadoop.bash
+++ lib/app/hadoop.bash
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+if [ -z "$(which hdfs)" ]; then
+  EXIT_CODE=1
+  return
+fi
+
 # Hadoop settings
-export HADOOP_HOME=/usr/local/Cellar/hadoop/2.4.1
-export HADOOP_CONF_DIR=$HADOOP_HOME/libexec/etc/hadoop
+export HADOOP_HOME=${HADOOP_HOME:-"/usr/local/Cellar/hadoop/2.4.1"}
+export HADOOP_CONF_DIR=${HADOOP_CONF_DIR:-"$HADOOP_HOME/libexec/etc/hadoop"}
 
 # PIG
-export PIG_HOME=/usr/local/Cellar/pig/0.12.0
+export PIG_HOME=${PIG_HOME:-"/usr/local/Cellar/pig/0.12.0"}
