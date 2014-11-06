d3dc4ae756746de41529eab511c8663753e55205
ansverma <ansverma@adobe.com>
2014-11-05 16:42:36 -0800

    git app should be env specific

--- lib/git.bash
+++ lib/git.bash
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# git auto complete
-source $CELLAR_PATH/git/2.1.1/etc/bash_completion.d/git-completion.bash
+if [ $PLATFORM == 'OSX' ]; then
+  # git auto complete
+  source $CELLAR_PATH/git/2.1.1/etc/bash_completion.d/git-completion.bash
 
-# git prompt
-source $CELLAR_PATH/git/2.1.1/etc/bash_completion.d/git-prompt.sh
+  # git prompt
+  source $CELLAR_PATH/git/2.1.1/etc/bash_completion.d/git-prompt.sh
+fi
