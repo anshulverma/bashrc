e568ad0555aab265375f81a24b267aae9b1fc81b
ansverma <ansverma@adobe.com>
2014-11-05 17:03:59 -0800

    only configure java if it is installed

--- lib/app/java.bash
+++ lib/app/java.bash
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ -z "$(which java)" ]; then
+  EXIT_CODE=1
+  return
+fi
+
 export JAVA_HOME=${JAVA_HOME:-"$(/usr/libexec/java_home -v 1.7)"}
 export GROOVY_HOME=${GROOVY_HOME:-"/usr/local/opt/groovy/libexec"}
 export MAVEN_OPTS="-Xmx2048m -XX:MaxPermSize=2048m"
