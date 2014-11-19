e9dbc02983283709e848c60de8dc4b1e6bb9616e
Anshul Verma <anshul.verma86@gmail.com>
2014-11-19 13:18:03 -0800

    adding ability to log prompt status

--- lib/prompt.bash
+++ lib/prompt.bash
@@ -29,17 +29,39 @@ function pcolor() {
   fi
 }
 
-function print_git_branch() {
+function git_color() {
   git branch &>/dev/null;
   if [ $? -eq 0 ]; then
     git status | grep "nothing to commit" > /dev/null 2>&1
     if [ "$?" -eq "0" ]; then
       # clean repository - nothing to commit
-      printf "$(pcolor $Green)$(__git_ps1 "(%s)")$(pcolor $ResetColor) "
+      echo $Green
     else
       # changes to working tree
-      printf "$(pcolor $IRed)$(__git_ps1 "(%s)")$(pcolor $ResetColor) "
+      echo $IRed
     fi
+  fi
+}
+
+function print_git_branch() {
+  git branch &>/dev/null;
+  if [ $? -eq 0 ]; then
+    local git_color=$(git_color)
+    printf "$(pcolor $git_color)$(__git_ps1 "(%s)")$(pcolor $ResetColor) "
+  fi
+}
+
+function get_git_status() {
+  git branch &>/dev/null;
+  if [ $? -eq 0 ]; then
+    git status | grep "nothing to commit" > /dev/null 2>&1
+    if [ "$?" -eq "0" ]; then
+      echo "git repository is clean"
+    else
+      echo "git repository is dirty"
+    fi
+  else
+    echo "not in a git repository"
   fi
 }
 
@@ -64,22 +86,29 @@ function print_end_with_job_info() {
 # Test connection type:
 if running_in_docker; then
   CNX=${Yellow}       # Logged into a docker container
+  PROMPT_STATUS_CNX="logged into docker"
 elif [ -n "${SSH_CONNECTION}" ]; then
   CNX=${Green}        # Connected on remote machine, via ssh (good).
+  PROMPT_STATUS_CNX="logged into remote machine via ssh"
 elif [[ "${DISPLAY%%:0*}" != "" ]]; then
   CNX=${ALERT}        # Connected on remote machine, not via ssh (bad).
+  PROMPT_STATUS_CNX="you are in a remote machine but not via ssh"
 else
   CNX=${Cyan}         # Connected on local machine.
+  PROMPT_STATUS_CNX="connected to local machine"
 fi
 
 # Test user type:
 USER=${USER:-"$(whoami)"}
 if [[ ${USER} == "root" ]]; then
   SU=${Red}           # User is root.
+  PROMPT_STATUS_SU="you are root"
 elif [[ ${USER} != $LOGNAME ]]; then
   SU=${BRed}          # User is not login user.
+  PROMPT_STATUS_SU="you are not a login user"
 else
   SU=${Cyan}          # User is normal (well ... most of us are).
+  PROMPT_STATUS_SU="you are a normal user"
 fi
 
 # Number of CPU
@@ -117,12 +146,38 @@ function load_color() {
   fi
 }
 
+# Returns a string explaining load info
+function get_load_status() {
+  local SYSLOAD=$(load)
+  if [ ${SYSLOAD} -gt ${XLOAD} ]; then
+    echo "machine is overloaded"
+  elif [ ${SYSLOAD} -gt ${MLOAD} ]; then
+    echo -en ${Red}
+    echo "load is very high"
+  elif [ ${SYSLOAD} -gt ${SLOAD} ]; then
+    echo "load is high"
+  else
+    echo "load is normal"
+  fi
+}
+
 # Returns a color according to running/suspended jobs.
 function job_color() {
   if [ $(jobs -s | wc -l) -gt "0" ]; then
     echo -en ${BRed}
   elif [ $(jobs -r | wc -l) -gt "0" ] ; then
     echo -en ${BCyan}
+  fi
+}
+
+# Returns job info as a status string
+function get_job_info_status() {
+  if [ $(jobs -s | wc -l) -gt "0" ]; then
+    echo "background jobs exist that are stopped"
+  elif [ $(jobs -r | wc -l) -gt "0" ] ; then
+    echo "background jobs exist that are running"
+  else
+    echo "background jobs do not exist"
   fi
 }
 
@@ -141,6 +196,15 @@ function build_prompt() {
   printf ' '
   print_end_with_job_info
   printf ' '
+}
+
+function prompt_status_log() {
+  echo -e $(git_color)$(get_git_status)
+  echo -e ${SU}${PROMPT_STATUS_SU}
+  echo -e ${CNX}${PROMPT_STATUS_CNX}
+  echo -e $(load_color)$(get_load_status)${ResetColor}
+  echo -e "you are in "$PWD$
+  echo -e $(job_color)$(get_job_info_status)${ResetColor}
 }
 
 export PROMPT_COMMAND='
