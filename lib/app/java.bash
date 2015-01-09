dbc263b8bd2de3c99255dcdba5e7b559a05fa8d1
Anshul Verma <anshul.verma86@gmail.com>
2015-01-09 15:21:40 -0800

    java home is only set if binary is present

--- lib/app/java.bash
+++ lib/app/java.bash
@@ -5,6 +5,9 @@ if [ -z "$(which java)" ]; then
   return
 fi
 
-export JAVA_HOME=${JAVA_HOME:-"$(/usr/libexec/java_home -v 1.7)"}
+if [ -x "/usr/libexec/java_home" ]; then
+  export JAVA_HOME=${JAVA_HOME:-"$(/usr/libexec/java_home -v 1.7)"}
+fi
+
 export GROOVY_HOME=${GROOVY_HOME:-"/usr/local/opt/groovy/libexec"}
 export MAVEN_OPTS="-Xmx2048m -XX:MaxPermSize=2048m"
