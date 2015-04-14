964418ceb3a8d56f499ff5bed55fb80742cc24b2
Anshul Verma <anshul.verma86@gmail.com>
2015-04-14 09:28:36 -0700

    load a custom init file first to allow to override QUIET_MODE

--- lib/init.bash
+++ lib/init.bash
@@ -6,7 +6,13 @@ export PATH="/usr/local/bin:$PATH"                          # make sure brew's s
 export PATH="$PATH:/opt/openresty/nginx/sbin"               # add nginx
 export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH" # really want to use coreutils
 
-QUIET_MODE=${QUIET_MODE:-true}
-if `tty > /dev/null`; then
-  QUIET_MODE=false
+if [ -f "$HOME/.bashrc_init" ]; then
+  source $HOME/.bashrc_init
+fi
+
+if [ "$QUIET_MODE" != "true" ]; then # forced quiet mode
+  QUIET_MODE=${QUIET_MODE:-true}
+  if `tty > /dev/null`; then
+    QUIET_MODE=false
+  fi
 fi
