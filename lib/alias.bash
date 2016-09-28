ceb5b9921c7aed659dd588f8bb888fe20a355b94
Anshu Verma <averma@nfmd-averma5sf.corp.netflix.com>
2016-09-28 09:23:20 -0700

    alias ls commands for gnu

--- lib/alias.bash
+++ lib/alias.bash
@@ -23,15 +23,15 @@ alias egrep='egrep --color=auto'
 # The 'ls' family (this assumes you use a recent GNU ls).
 #-------------------------------------------------------------
 # Add colors for filetype and  human-readable sizes by default on 'ls':
-alias ls='ls -h --color'
-alias lx='ls -lXB'         #  Sort by extension.
-alias lk='ls -lSr'         #  Sort by size, biggest last.
-alias lt='ls -ltr'         #  Sort by date, most recent last.
-alias lc='ls -ltcr'        #  Sort by/show change time,most recent last.
-alias lu='ls -ltur'        #  Sort by/show access time,most recent last.
+alias ls='gls -h --color'
+alias lx='gls -lXB'         #  Sort by extension.
+alias lk='gls -lSr'         #  Sort by size, biggest last.
+alias lt='gls -ltr'         #  Sort by date, most recent last.
+alias lc='gls -ltcr'        #  Sort by/show change time,most recent last.
+alias lu='gls -ltur'        #  Sort by/show access time,most recent last.
 
 # The ubiquitous 'll': directories first, with alphanumeric sorting:
-alias ll="ls -lv --group-directories-first"
+alias ll="gls -lv --group-directories-first"
 alias lm='ll |more'        #  Pipe through 'more'
 alias lr='ll -R'           #  Recursive ls.
 alias la='ll -A'           #  Show hidden files.
