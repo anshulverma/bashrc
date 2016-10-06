33b33cf2b07df93784d175c28dd68a260b2a2389
Anshul Verma <averma@netflix.com>
2016-10-06 12:45:22 -0700

    enable iterm2 shell integration for bash

--- lib/app/term.bash
+++ lib/app/term.bash
@@ -3,3 +3,6 @@
 if [[ -n ${TMUX} ]];then
   TERM=screen-256color
 fi
+
+# source iterm2 shell integration if available
+test -e "${HOME}/.iterm2_shell_integration.bash" && source "${HOME}/.iterm2_shell_integration.bash"
